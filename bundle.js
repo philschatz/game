238e3a4bb371741a9b6351c76bec5ff501b0c74b
Philip Schatz <phil@cnx.org>
2014-09-14 18:18:21 -0400

    rebuild bundle

--- bundle.js
+++ bundle.js
@@ -48981,157 +48981,189 @@ module.exports = function(other, bbox, vec, resting) {
 
 });
 
-require.define("/my-map.js",function(require,module,exports,__dirname,__filename,process,global){var defaultMap = require('./default-map')
-var mapFromHash = require('./map-from-hash')
+require.define("/maps/my.coffee",function(require,module,exports,__dirname,__filename,process,global){(function() {
+  var lighthouseMap, mapFromHash, mapInfo;
 
+  lighthouseMap = require("./lighthouse");
 
-if (window.location.hash.length > 2) {
-  mapInfo = mapFromHash();
-} else {
-  mapInfo = defaultMap;
-}
+  mapFromHash = require("./from-hash");
 
+  if (window.location.hash.length > 2) {
+    mapInfo = {
+      map: mapFromHash(),
+      textures: lighthouseMap.textures
+    };
+  } else {
+    mapInfo = lighthouseMap;
+  }
 
-module.exports = mapInfo;
+  module.exports = mapInfo;
+
+}).call(this);
 
 });
 
-require.define("/default-map.js",function(require,module,exports,__dirname,__filename,process,global){colorMapper = function(hex) {
-  switch (hex) {
-    case '000000':
-    case 'ffffff': return false; break;
-    case '21bf64': return 1;
-    case '-bf40c': return false; // black
-    case 'f2f2f2': return false;
-    case '278bce': return 2;
-    case '273c51': return 4;
-    case 'd97115': return 6; break; // goal color
-    default: console.log('Oops, need to match ' + hex); return 5;
-  }
-}
+require.define("/maps/lighthouse.coffee",function(require,module,exports,__dirname,__filename,process,global){(function() {
+  var fromHash, hash, map, newColor, origColor, playerPosition, textureVariantAry, textureVariantColors, textureVariants, textures, variantMapper, variantName, variantNames, _i, _j, _len, _len1;
 
+  fromHash = require('./from-hash');
 
-var THE_MAP = window.THE_MAP = {};
+  textures = ['stone', ['grass', 'grass', 'square-light-pyramid-in'], 'square-medium-pyramid-in', 'square-dark-pyramid-in', ['grass', 'grass', 'square-grasstop-light-pyramid-out'], 'bridge-post-top', 'bridge', 'bridge-post', 'obelisk-3', 'obelisk-2', 'obelisk-1', 'lighthouse-red', 'lighthouse-white'];
 
-var position = {x:0, y:0, z:0}; // Offset for the map loader
+  textureVariants = [null, ['square-light-pyramid-in', 'square-light-pyramid-out', 'square-light-square-in', 'square-light-square-out'], ['square-medium-pyramid-in', 'square-medium-pyramid-out', 'square-medium-square-in', 'square-medium-square-out'], ['square-dark-pyramid-in', 'square-dark-pyramid-out', 'square-dark-square-big', 'square-dark-square-in'], ['square-grasstop-light-pyramid-out', 'square-grasstop-light-square-out']];
 
-addVoxel = function(voxel, c) {
-  var x = '' + voxel[0];
-  var y = '' + voxel[1];
-  var z = '' + voxel[2];
+  textureVariantColors = [];
 
-  if (THE_MAP[x] == null) THE_MAP[x] = {};
-  if (THE_MAP[x][y] == null) THE_MAP[x][y] = {};
-  THE_MAP[x][y][z] = c;
-}
+  newColor = textures.length;
 
-var voxels = [[-1,0,-1,"21bf64"],[-1,0,0,"21bf64"],[0,0,0,"21bf64"],[0,0,-1,"21bf64"],[0,0,-2,"21bf64"],[-1,0,-2,"21bf64"],[-2,0,-1,"21bf64"],[-2,0,0,"21bf64"],[-1,0,1,"21bf64"],[0,0,1,"21bf64"],[1,0,0,"21bf64"],[1,0,-1,"21bf64"],[9,0,-3,"-bf40c"],[9,1,-3,"-bf40c"],[9,2,-3,"-bf40c"],[8,0,9,"-bf40c"],[8,1,9,"-bf40c"],[8,2,9,"-bf40c"],[6,0,-10,"-bf40c"],[6,1,-10,"-bf40c"],[6,2,-10,"-bf40c"],[6,3,-10,"-bf40c"],[6,4,-10,"-bf40c"],[6,5,-10,"-bf40c"],[-10,0,-10,"-bf40c"],[-10,1,-10,"-bf40c"],[-10,2,-10,"-bf40c"],[-10,3,-10,"-bf40c"],[-10,4,-10,"-bf40c"],[-10,5,-10,"-bf40c"],[-10,6,-10,"-bf40c"],[-10,7,-10,"-bf40c"],[-10,8,-10,"-bf40c"],[-10,9,-10,"21bf64"],[-10,9,-9,"21bf64"],[-10,9,-2,"21bf64"],[-10,9,1,"21bf64"],[-10,9,6,"21bf64"],[-9,9,0,"-bf40c"],[-8,9,0,"-bf40c"],[-7,9,0,"-bf40c"],[-6,9,0,"-bf40c"],[-5,9,0,"-bf40c"],[-4,9,0,"-bf40c"],[-3,9,0,"-bf40c"],[-9,9,2,"-bf40c"],[-8,9,2,"-bf40c"],[-7,9,2,"-bf40c"],[-6,9,2,"-bf40c"],[-5,9,2,"-bf40c"],[-4,9,2,"-bf40c"],[-3,9,2,"-bf40c"],[-2,9,2,"-bf40c"],[-1,9,2,"-bf40c"],[0,9,2,"-bf40c"],[1,9,2,"-bf40c"],[2,9,2,"-bf40c"],[3,9,2,"-bf40c"],[4,9,2,"-bf40c"],[5,9,2,"-bf40c"],[6,9,2,"-bf40c"],[7,9,2,"-bf40c"],[8,9,2,"-bf40c"],[9,9,2,"21bf64"],[-9,9,-3,"-bf40c"],[-8,9,-3,"-bf40c"],[-7,9,-3,"-bf40c"],[-6,9,-3,"-bf40c"],[-5,9,-3,"21bf64"],[-9,9,-5,"-bf40c"],[-8,9,-5,"-bf40c"],[-7,9,-5,"-bf40c"],[-6,9,-5,"-bf40c"],[-5,9,-5,"-bf40c"],[-4,9,-5,"-bf40c"],[-3,9,-5,"-bf40c"],[-2,9,-5,"-bf40c"],[-1,9,-5,"21bf64"],[0,9,-5,"21bf64"],[-9,9,-6,"-bf40c"],[-8,9,-6,"-bf40c"],[-7,9,-6,"21bf64"],[-9,9,-8,"-bf40c"],[-8,9,-8,"-bf40c"],[-7,9,-8,"-bf40c"],[-6,9,-8,"-bf40c"],[-5,9,-8,"-bf40c"],[-4,9,-8,"-bf40c"],[-3,9,-8,"-bf40c"],[-2,9,-8,"-bf40c"],[-1,9,-8,"-bf40c"],[0,9,-8,"-bf40c"],[1,9,-8,"-bf40c"],[2,9,-8,"-bf40c"],[3,9,-8,"-bf40c"],[4,9,-8,"-bf40c"],[5,9,-8,"-bf40c"],[6,9,-8,"-bf40c"],[7,9,-8,"21bf64"],[4,9,-7,"21bf64"],[2,9,3,"21bf64"],[-4,9,3,"-bf40c"],[-4,9,4,"-bf40c"],[-4,9,5,"-bf40c"],[-3,9,4,"21bf64"],[-5,9,5,"21bf64"],[-4,9,6,"-bf40c"],[-4,9,7,"-bf40c"],[-4,9,8,"-bf40c"],[-3,9,7,"21bf64"],[-4,9,9,"21bf64"],[1,9,3,"-bf40c"],[1,9,4,"-bf40c"],[1,9,5,"-bf40c"],[1,9,6,"-bf40c"],[1,9,7,"-bf40c"],[1,9,8,"21bf64"],[-1,9,-4,"21bf64"],[8,3,9,"278bce"],[7,3,9,"278bce"],[6,3,9,"278bce"],[5,3,9,"278bce"],[9,3,-3,"278bce"],[9,3,-2,"278bce"],[9,3,-4,"278bce"],[6,6,-10,"273c51"],[5,6,-10,"273c51"],[-1,9,0,"d97115"],[0,9,0,"d97115"],[-1,9,-1,"d97115"],[0,9,-1,"d97115"],[-1,10,0,"d97115"],[-1,10,-1,"d97115"],[0,10,0,"d97115"],[0,10,-1,"d97115"],[1,9,0,"d97115"],[1,9,-1,"d97115"],[1,9,1,"d97115"],[0,9,1,"d97115"],[-1,9,1,"d97115"],[1,9,-2,"d97115"],[-1,9,-2,"d97115"],[0,9,-2,"d97115"],[-2,9,-2,"d97115"],[-2,9,-1,"d97115"],[-2,9,0,"d97115"],[-2,9,1,"d97115"]];var dimensions = [19,10,19];
-
-voxels.map(function(voxel) {if (colorMapper(voxel[3])) { addVoxel([position.x + voxel[0], position.y + voxel[1], position.z + voxel[2]], colorMapper(voxel[3])) }});
-
-module.exports = {
-  textures: [
-    ['grass', 'dirt', 'grass_dirt'],
-    'obsidian',
-    'brick',
-    'grass',
-    'plank',
-    'whitewool',
-    'whitewool-l',
-    'whitewool-r',
-    'whitewool-t',
-    'whitewool-b'
-  ],
-  map: function(i,j,k) {
-    var x = THE_MAP['' + i];
-    if (!x) { return 0; }
-    var y = x['' + j];
-    if (!y) { return 0; }
-    var z = y['' + k];
-    return z || 0;
-  }
-}
-
-});
-
-require.define("/map-from-hash.js",function(require,module,exports,__dirname,__filename,process,global){defaultMap = require('./default-map');
-
-var THE_MAP = window.THE_MAP = {};
-
-addVoxel = function(x, y, z, c) {
-  c = c + 1;
-
-  var x = '' + x;
-  var y = '' + y;
-  var z = '' + z;
-
-  if (THE_MAP[x] == null) THE_MAP[x] = {};
-  if (THE_MAP[x][y] == null) THE_MAP[x][y] = {};
-  THE_MAP[x][y][z] = c;
-}
-
-getVoxel = function(i,j,k) {
-  var x = THE_MAP['' + i];
-  if (!x) { return 0; }
-  var y = x['' + j];
-  if (!y) { return 0; }
-  var z = y['' + k];
-  return z || 0;
-}
-
-// https://gist.github.com/665235
-function decode( string ) {
-  var output = []
-  string.split('').forEach( function ( v ) { output.push( "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".indexOf( v ) ) } )
-  return output
-}
-
-function buildFromHash() {
-  var hashMask = null;
-
-  var hash = window.location.hash.substr( 1 ),
-  hashChunks = hash.split(':'),
-  chunks = {}
-
-  animationFrames = []
-  for( var j = 0, n = hashChunks.length; j < n; j++ ) {
-    chunk = hashChunks[j].split('/')
-    chunks[chunk[0]] = chunk[1]
-  }
-
-  if ( chunks['C'] )
-  {
-    // Ignore colors
-  }
-  var frameMask = 'A'
-
-  if ( (!hashMask || hashMask == frameMask) && chunks[frameMask] ) {
-    // decode geo
-    var current = { x: 0, y: 0, z: 0, c: 0 }
-    var data = decode( chunks[frameMask] )
-    var i = 0, l = data.length
-
-    while ( i < l ) {
-
-      var code = data[ i ++ ].toString( 2 )
-      if ( code.charAt( 1 ) == "1" ) current.x += data[ i ++ ] - 32
-      if ( code.charAt( 2 ) == "1" ) current.y += data[ i ++ ] - 32
-      if ( code.charAt( 3 ) == "1" ) current.z += data[ i ++ ] - 32
-      if ( code.charAt( 4 ) == "1" ) current.c += data[ i ++ ] - 32
-      if ( code.charAt( 0 ) == "1" ) {
-        addVoxel(current.x, current.y, current.z, current.c)
+  for (origColor = _i = 0, _len = textureVariants.length; _i < _len; origColor = ++_i) {
+    variantNames = textureVariants[origColor];
+    if (variantNames) {
+      textureVariantAry = [];
+      for (_j = 0, _len1 = variantNames.length; _j < _len1; _j++) {
+        variantName = variantNames[_j];
+        textures.push(variantName);
+        textureVariantAry.push(newColor);
+        newColor++;
       }
+      textureVariantColors.push(textureVariantAry);
+    } else {
+      textureVariantColors.push(null);
     }
   }
-}
 
-module.exports = function() {
-  buildFromHash();
-  return {
-    map: getVoxel,
-    textures: defaultMap.textures
-  }
-};
+  console.log('textures:', textures);
+
+  console.log('textureVariantColors: ', textureVariantColors);
+
+  playerPosition = [1, 7, 3];
+
+  hash = ':A/aWpaieaePaiiaiiammaiiakkaiiaSdaoYakcaieaXoShahfbbhhYhShYfShYhShYfShYhYhSfYhShYhSfYhShYhSfYhShYhSfYhShSeYfSfYhSfYfSfYhSfYfSfYhSfYfahfYfYfShYfSfYfShYfSfYfShYfSfYfShYfSfSiYhShYfShYhWhhYfYfShYhYhShYfYfShYhYhShYfYfShYhYhYhSfSfYhShShYhSfSfYhShShYhSfaihSfSfahhSfaffYhSfYfSfYhaffYhaffYhaffYhSfYfahfYfafhSfYfShYfSfYfShYfSfYfShaffShaffSiafeahhYfaihYeaihShYfSfYfShaqmfYhbiShShYhYhYhYhYhYhaadSfSfSfamlSfSfSfSfabfYhYhYhYhYhcahShShShShShShYhYhYhYhYhYhSfSfSfSfSfSfYfYfYfYfYfbeifSfSfSfSfahhSfahhSfajhSfahhSfahhSfahhSfSiShShSkShShffeffShShafefhihhSfYcShaffShaffShafeShShUhSfSfbhfhSfSfbfffSfSfSfSfYhYhYjYhYhYhaahYhYhYhaefYhakhShShShShShShShShShaffShbfehYfThfShaffShYfYfYfcfhSfSfSfSfSfSfSfSfSfSfYhYhYhYhYhYhYhYhShShShShShShShShShShaffYfahhYfYfYfYfYfYfbfhfShYfSfSfSeSfSfSbSfSfYhYhYmaXtSfSfSeSfSfSbSfSfYoajhSfYhafiShSkShShShShShShYhaefShaffShaffShaffShadfShaffShddhjSfSfSfSfSfSfSfSfSfSfSfSfSfYhYhYhYhYhYhYhYhYhYhYhYhYhShShShShShShShShShShShShShYfYfYfYfYfYfYfYfYfYfYfYfSfSfSfSfSfSfSfSfSfSfSfSfahrSfSfSfSfSfSfSfSfSfSfSfahrSfSfSfSfSfSfSfSfSfSfSffaalcYhYhShYfYfappYhfYmVmYfYfYfTfhVheVeiUfUfUfUfUfUfUfdcpefjfhhYfYfYfXYfhUhUhUhUhUhUhUhUhccYUhUhUhUhUhUhUhUhVhefjfhhYfYfYfShYhYhYhYhYhYhYhYhYhYhYhShYfYfYfYfYfYfYfYfYfYfYffoYihUhUhUhUhUhUhUhUhVhedcXiUhUhUhUhUhUhUhUhVhedcYiUfUiUhUhUhUhUhUhVheXZhbShShShShShShShShShShShShShYfYfYfYfYfUhUhUhUhVikShShShShShXfbdShShShShShfehbjYfYfYfdkfdYfYeYfajhZfhUfYhYhYdajhYfYfYfajhYeYfajhYeYffiffhYfahfYfehhhfehffbfffbhihZffShShYkYfYfYfffhcjYhYhYhYhShYfYfYfYfShYhYhYhYhShYfYfYfYfShYhYhYhYhfehekVhhVhhfiddabmRiYhYhYhYhYhYhYhYhYhYhYhblkeSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShahVShShShShShShShShShShShYhSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShYhSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShYhSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShfahZnYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShSh';
+
+  variantMapper = function(c) {
+    var newC, variants;
+    variants = textureVariantColors[c];
+    if (variants) {
+      newC = variants[Math.floor(Math.random() * variants.length)];
+      return newC;
+    } else {
+      return c;
+    }
+  };
+
+  map = fromHash(hash, variantMapper);
+
+  module.exports = {
+    map: map,
+    textures: textures,
+    playerPosition: playerPosition
+  };
+
+}).call(this);
+
+});
+
+require.define("/maps/from-hash.coffee",function(require,module,exports,__dirname,__filename,process,global){(function() {
+  var THE_MAP, addVoxel, buildFromHash, decode, getVoxel;
+
+  THE_MAP = window.THE_MAP = {};
+
+  addVoxel = function(x, y, z, c) {
+    if (!c) {
+      return;
+    }
+    c = c + 1;
+    x = '' + x;
+    y = '' + y;
+    z = '' + z;
+    if (THE_MAP[x] == null) {
+      THE_MAP[x] = {};
+    }
+    if (THE_MAP[x][y] == null) {
+      THE_MAP[x][y] = {};
+    }
+    THE_MAP[x][y][z] = c;
+  };
+
+  getVoxel = function(i, j, k) {
+    var x, y, z;
+    x = THE_MAP['' + i];
+    if (!x) {
+      return 0;
+    }
+    y = x['' + j];
+    if (!y) {
+      return 0;
+    }
+    z = y['' + k];
+    return z || 0;
+  };
+
+  decode = function(string) {
+    var output;
+    output = [];
+    string.split('').forEach(function(v) {
+      output.push('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.indexOf(v));
+    });
+    return output;
+  };
+
+  buildFromHash = function(hash, variantChooser) {
+    var animationFrames, chunk, chunks, code, color, current, data, frameMask, hashChunks, hashMask, i, j, l, n;
+    if (hash == null) {
+      hash = window.location.hash.substr(1);
+    }
+    hashMask = null;
+    hashChunks = hash.split(':');
+    chunks = {};
+    animationFrames = [];
+    n = hashChunks.length;
+    j = 0;
+    while (j < n) {
+      chunk = hashChunks[j].split('/');
+      chunks[chunk[0]] = chunk[1];
+      j++;
+    }
+    frameMask = 'A';
+    if ((!hashMask || hashMask === frameMask) && chunks[frameMask]) {
+      current = {
+        x: 0,
+        y: 0,
+        z: 0,
+        c: 0
+      };
+      data = decode(chunks[frameMask]);
+      i = 0;
+      l = data.length;
+      while (i < l) {
+        code = data[i++].toString(2);
+        if (code.charAt(1) === '1') {
+          current.x += data[i++] - 32;
+        }
+        if (code.charAt(2) === '1') {
+          current.y += data[i++] - 32;
+        }
+        if (code.charAt(3) === '1') {
+          current.z += data[i++] - 32;
+        }
+        if (code.charAt(4) === '1') {
+          current.c += data[i++] - 32;
+        }
+        if (code.charAt(0) === '1') {
+          color = current.c;
+          if (variantChooser) {
+            color = variantChooser(color);
+          }
+          addVoxel(current.x, current.y, current.z, color);
+        }
+      }
+    }
+    return getVoxel;
+  };
+
+  module.exports = buildFromHash;
+
+}).call(this);
 
 });
 
@@ -49156,7 +49188,7 @@ require.define("/index.coffee",function(require,module,exports,__dirname,__filen
 
   collideTerrain = require('./collide-terrain');
 
-  mapConfig = require('./my-map');
+  mapConfig = require('./maps/my');
 
   myMap = mapConfig.map;
 
@@ -49172,7 +49204,7 @@ require.define("/index.coffee",function(require,module,exports,__dirname,__filen
 
   view.camera.position.z = 1000;
 
-  view.camera.scale.set(.5, .5, .5);
+  view.camera.scale.set(.85, .85, .85);
 
   Number.prototype.mod = function(n) {
     return ((this % n) + n) % n;
@@ -49218,7 +49250,7 @@ require.define("/index.coffee",function(require,module,exports,__dirname,__filen
 
   substack.possess();
 
-  initialCoords = [0, 5, 0];
+  initialCoords = mapConfig.playerPosition || [0, 5, 0];
 
   substack.yaw.position.set(initialCoords[0], initialCoords[1], initialCoords[2]);
 
