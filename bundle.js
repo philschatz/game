91956e4f8631476e8d45240608ae2aa81e394ff3
Philip Schatz <phil@cnx.org>
2014-09-14 22:14:49 -0400

    rebuild bundle

--- bundle.js
+++ bundle.js
@@ -48860,124 +48860,135 @@ View.prototype.appendTo = function(element) {
 
 });
 
-require.define("/package.json",function(require,module,exports,__dirname,__filename,process,global){module.exports = {}
+require.define("/collisions/terrain.coffee",function(require,module,exports,__dirname,__filename,process,global){(function() {
+  var GameManager;
+
+  GameManager = require('../actions/game-manager');
+
+  module.exports = function(other, bbox, vec, resting) {
+    var axes, hit, self, vec3;
+    self = this;
+    axes = ['x', 'y', 'z'];
+    vec3 = [vec.x, vec.y, vec.z];
+    hit = function(axis, tile, coords, dir, edge) {
+      var belowBlockDepth, blockDepth, isBehind, isBelowTopCollide, isCameraAxis, isInside, isVectorAxis, multiplier, myBlock, newCoords, newDepth, perpendicAxis, scaleJustToBeSafe, y, _ref;
+      newDepth = null;
+      scaleJustToBeSafe = 1.5;
+      y = coords[1];
+      _ref = GameManager.get2DInfo(), perpendicAxis = _ref.perpendicAxis, multiplier = _ref.multiplier;
+      blockDepth = GameManager.get2DBlock(coords);
+      belowBlockDepth = GameManager.get2DBlock([coords[0], y - 1, coords[2]]);
+      myBlock = GameManager.getPlayerBlock();
+      isCameraAxis = GameManager.isCameraAxis(axis);
+      isVectorAxis = vec3[axis] !== 0;
+      isBehind = function(depth) {
+        return multiplier * (bbox.base[perpendicAxis] - depth) < 0;
+      };
+      isInside = function(depth) {
+        return Math.floor(bbox.base[perpendicAxis]) === depth;
+      };
+      isBelowTopCollide = function(depth) {
+        return multiplier * (belowBlockDepth - depth) > 0;
+      };
+      if (isVectorAxis) {
+        if (axis === 1 && dir === -1) {
+          if ((blockDepth != null) && coords[1] !== Math.floor(bbox.base[1])) {
+            if (!tile) {
+              console.log('fallingg......');
+              newDepth = blockDepth;
+              tile = true;
+            }
+          }
+        } else if (isCameraAxis && !isBehind(myBlock) && (blockDepth != null)) {
+          console.log('walking and block next to me');
+          if ((isBehind(blockDepth) || isInside(blockDepth)) && isBelowTopCollide(blockDepth)) {
+            newDepth = blockDepth + multiplier;
+          }
+          tile = false;
+        } else if (isCameraAxis && isBehind(myBlock)) {
+          console.log('walking behind a block');
+          tile = false;
+        }
+      }
+      if ((newDepth != null) && newDepth !== this.controlling.aabb().base[perpendicAxis]) {
+        newCoords = this.controlling.aabb().base;
+        newCoords[perpendicAxis] = newDepth + .5;
+        console.log('moving from:', this.controlling.aabb().base);
+        console.log('moving to  :', newCoords);
+        this.controlling.moveTo(newCoords[0], newCoords[1], newCoords[2]);
+      }
+      if (!tile) {
+        return;
+      }
+      if (Math.abs(vec3[axis]) < Math.abs(edge)) {
+        return;
+      }
+      vec3[axis] = vec[axes[axis]] = edge;
+      other.acceleration[axes[axis]] = 0;
+      resting[axes[axis]] = dir;
+      other.friction[axes[(axis + 1) % 3]] = other.friction[axes[(axis + 2) % 3]] = (axis === 1 ? self.friction : 1);
+      return true;
+    };
+    this.collideVoxels(bbox, vec3, hit.bind(this));
+  };
+
+}).call(this);
+
 });
 
-require.define("/collide-terrain.js",function(require,module,exports,__dirname,__filename,process,global){// other -
-// bbox - player bbox
-// vec -
-// resting -
+require.define("/actions/game-manager.coffee",function(require,module,exports,__dirname,__filename,process,global){(function() {
+  var GameManager;
 
-// collideTerrain
-module.exports = function(other, bbox, vec, resting) {
-  var self = this
-  var axes = ['x', 'y', 'z']
-  var vec3 = [vec.x, vec.y, vec.z]
-  var hit = function (axis, tile, coords, dir, edge) {
-    /* Collision cases:
+  module.exports = new (GameManager = (function() {
+    function GameManager() {}
 
-    - I am falling and there is a flattened block below me: move my depth
-    - I am walking and there is a flattened block next to me: move me in front of that block (may start falling)
-    - walking and I am behind a block: do not move my depth
+    GameManager.prototype.getGame = function() {
+      return window.game;
+    };
 
-    */
-
-    var newDepth = null;
-
-    var cameraType = this.controlling.rotation.y / Math.PI * 2;
-    cameraType = Math.round(cameraType).mod(4);
-
-    var scaleJustToBeSafe = 1.5;
-    var cameraDir = 1;
-    var cameraAxis;
-    var cameraPerpendicAxis;
-    if (cameraType >= 2) { cameraDir = -1; }
-    if (cameraType.mod(2) == 0)  { cameraAxis = 0/*x*/; cameraPerpendicAxis = 2; }
-    else                         { cameraAxis = 2/*z*/; cameraPerpendicAxis = 0; }
-
-    y = coords[1];
-    blockDepth = this.sparseCollisionMap[cameraType]['' + coords[cameraAxis] + '|' + y];
-    belowBlockDepth = this.sparseCollisionMap[cameraType]['' + coords[cameraAxis] + '|' + (y - 1)];
-    myBlock = this.sparseCollisionMap[cameraType]['' + Math.floor(bbox.base[cameraAxis]) + '|' + y];
-
-
-    isCameraAxis = axis == cameraAxis;
-    isVectorAxis = vec3[axis] != 0;
-    // isDirOfMovement = dir * vec3[cameraAxis] > 0;
-
-
-    var isBehind = function(depth) {
-      return cameraDir * (bbox.base[cameraPerpendicAxis] - depth) < 0;
-    }
-
-    var isInside = function(depth) {
-      return Math.floor(bbox.base[cameraPerpendicAxis]) == depth;
-    }
-
-
-    /* Collision cases:
-
-    - I am falling and there is a flattened block below me: move my depth
-    - I am walking and there is a flattened block next to me: move me in front of that block
-    - walking and I am behind a block: do not move my depth
-
-    */
-
-    if (isVectorAxis) {
-      if (axis == 1 && dir == -1) {
-        // I am falling ...
-        if (blockDepth != null && coords[1] != Math.floor(bbox.base[1])) { // the last bit checks to make sure we are actually falling instead of just checking the current voxel where the player is.
-          // .. and there is a flattened block below me
-          // console.log('falling and block below');
-
-          // If I am going to land on ground then do not change the depth
-          if (!tile) {
-            console.log('fallingg......');
-            newDepth = blockDepth + 0.5;
-            tile = true; // HACK to tell the game there's a collision
-          }
-        }
+    GameManager.prototype.get2DInfo = function() {
+      var axis, dir, multiplier, perpendicAxis;
+      dir = this.getGame().controlling.rotation.y / Math.PI * 2;
+      dir = Math.round(dir).mod(4);
+      multiplier = 1;
+      if (dir >= 2) {
+        multiplier = -1;
       }
-
-      else if (isCameraAxis && !isBehind(myBlock) && blockDepth != null) {
-        // I am walking and there is a flattened block next to me
-        console.log('walking and block next to me');
-        if (isBehind(blockDepth) || isInside(blockDepth)) {
-          newDepth = blockDepth + cameraDir + 0.5
-        }
-        tile = false;
+      if (dir.mod(2) === 0) {
+        axis = 0;
+        perpendicAxis = 2;
+      } else {
+        axis = 2;
+        perpendicAxis = 0;
       }
+      return {
+        axis: axis,
+        perpendicAxis: perpendicAxis,
+        dir: dir,
+        multiplier: multiplier
+      };
+    };
 
-      else if (isCameraAxis && isBehind(myBlock)) {
-        // I am walking and I am behind a block
-        console.log('walking behind a block');
-        tile = false;
-      }
+    GameManager.prototype.get2DBlock = function(coords) {
+      var axis, dir, perpendicAxis, y, _ref;
+      _ref = this.get2DInfo(), axis = _ref.axis, perpendicAxis = _ref.perpendicAxis, dir = _ref.dir;
+      y = coords[1];
+      return this.getGame().sparseCollisionMap[dir]['' + Math.floor(coords[axis]) + '|' + y];
+    };
 
-    }
+    GameManager.prototype.getPlayerBlock = function() {
+      return this.get2DBlock(this.getGame().controlling.aabb().base);
+    };
 
-    if (newDepth != null && newDepth != this.controlling.aabb().base[cameraPerpendicAxis]) {
-      var newCoords = this.controlling.aabb().base;
-      newCoords[cameraPerpendicAxis] = newDepth;
-      console.log('moving from:', this.controlling.aabb().base);
-      console.log('moving to  :', newCoords);
-      this.controlling.moveTo(newCoords[0], newCoords[1], newCoords[2]);
-    }
+    GameManager.prototype.isCameraAxis = function(axis) {
+      return this.get2DInfo().axis === axis;
+    };
 
+    return GameManager;
 
-    if (!tile) { return; }
+  })());
 
-
-    // boilerplate code?
-    if (Math.abs(vec3[axis]) < Math.abs(edge)) return
-    vec3[axis] = vec[axes[axis]] = edge
-    other.acceleration[axes[axis]] = 0
-    resting[axes[axis]] = dir
-    other.friction[axes[(axis + 1) % 3]] = other.friction[axes[(axis + 2) % 3]] = axis === 1 ? self.friction  : 1
-    return true
-  };
-  this.collideVoxels(bbox, vec3, hit.bind(this));
-}
+}).call(this);
 
 });
 
@@ -49041,14 +49052,12 @@ require.define("/maps/lighthouse.coffee",function(require,module,exports,__dirna
   hash = ':A/aWpaieaePaiiaiiammaiiakkaiiaSdaoYakcaieaXoShahfbbhhYhShYfShYhShYfShYhYhSfYhShYhSfYhShYhSfYhShYhSfYhShSeYfSfYhSfYfSfYhSfYfSfYhSfYfahfYfYfShYfSfYfShYfSfYfShYfSfYfShYfSfSiYhShYfShYhWhhYfYfShYhYhShYfYfShYhYhShYfYfShYhYhYhSfSfYhShShYhSfSfYhShShYhSfaihSfSfahhSfaffYhSfYfSfYhaffYhaffYhaffYhSfYfahfYfafhSfYfShYfSfYfShYfSfYfShaffShaffSiafeahhYfaihYeaihShYfSfYfShaqmfYhbiShShYhYhYhYhYhYhaadSfSfSfamlSfSfSfSfabfYhYhYhYhYhcahShShShShShShYhYhYhYhYhYhSfSfSfSfSfSfYfYfYfYfYfbeifSfSfSfSfahhSfahhSfajhSfahhSfahhSfahhSfSiShShSkShShffeffShShafefhihhSfYcShaffShaffShafeShShUhSfSfbhfhSfSfbfffSfSfSfSfYhYhYjYhYhYhaahYhYhYhaefYhakhShShShShShShShShShaffShbfehYfThfShaffShYfYfYfcfhSfSfSfSfSfSfSfSfSfSfYhYhYhYhYhYhYhYhShShShShShShShShShShaffYfahhYfYfYfYfYfYfbfhfShYfSfSfSeSfSfSbSfSfYhYhYmaXtSfSfSeSfSfSbSfSfYoajhSfYhafiShSkShShShShShShYhaefShaffShaffShaffShadfShaffShddhjSfSfSfSfSfSfSfSfSfSfSfSfSfYhYhYhYhYhYhYhYhYhYhYhYhYhShShShShShShShShShShShShShYfYfYfYfYfYfYfYfYfYfYfYfSfSfSfSfSfSfSfSfSfSfSfSfahrSfSfSfSfSfSfSfSfSfSfSfahrSfSfSfSfSfSfSfSfSfSfSffaalcYhYhShYfYfappYhfYmVmYfYfYfTfhVheVeiUfUfUfUfUfUfUfdcpefjfhhYfYfYfXYfhUhUhUhUhUhUhUhUhccYUhUhUhUhUhUhUhUhVhefjfhhYfYfYfShYhYhYhYhYhYhYhYhYhYhYhShYfYfYfYfYfYfYfYfYfYfYffoYihUhUhUhUhUhUhUhUhVhedcXiUhUhUhUhUhUhUhUhVhedcYiUfUiUhUhUhUhUhUhVheXZhbShShShShShShShShShShShShShYfYfYfYfYfUhUhUhUhVikShShShShShXfbdShShShShShfehbjYfYfYfdkfdYfYeYfajhZfhUfYhYhYdajhYfYfYfajhYeYfajhYeYffiffhYfahfYfehhhfehffbfffbhihZffShShYkYfYfYfffhcjYhYhYhYhShYfYfYfYfShYhYhYhYhShYfYfYfYfShYhYhYhYhfehekVhhVhhfiddabmRiYhYhYhYhYhYhYhYhYhYhYhblkeSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShahVShShShShShShShShShShShYhSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShYhSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShYhSfSfSfSfSfSfSfSfSfSfSfYhShShShShShShShShShShShfahZnYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShShXhhhYfYfYfSfSfSfYhYhYhShShXhhfYfYfYfSfSfSfYhYhYhShSh';
 
   variantMapper = function(c) {
-    var newC, variants;
+    var variants;
     variants = textureVariantColors[c];
-    if (variants) {
-      newC = variants[Math.floor(Math.random() * variants.length)];
-      return newC;
-    } else {
+    if (!variants) {
       return c;
     }
+    return variants[Math.floor(Math.random() * variants.length)];
   };
 
   map = fromHash(hash, variantMapper);
@@ -49186,7 +49195,7 @@ require.define("/index.coffee",function(require,module,exports,__dirname,__filen
 
   voxelView = require('voxel-view');
 
-  collideTerrain = require('./collide-terrain');
+  collideTerrain = require('./collisions/terrain');
 
   mapConfig = require('./maps/my');
 
